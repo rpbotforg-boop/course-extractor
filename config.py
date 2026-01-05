17ea2ceb0e20647d49d6197d3e5b6632a0ac9802
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-05 20:37:50 +0530

    Update configuration settings in config.py
    Updated configuration values for BOT_TOKEN, API_ID, and API_HASH.

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
-    API_ID = int(os.environ.get("API_ID", '27433400'))
-    API_HASH = os.environ.get("API_HASH", '1a286620de5ffe0a7d9b57e604293555')
-    AUTH_USER = os.environ.get('AUTH_USERS', '6201066540').split(',')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", '8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4')
+    API_ID = int(os.environ.get("API_ID", '22447622'))
+    API_HASH = os.environ.get("API_HASH", '543b62d58d3e723e766ba57a984ab65d')
+    AUTH_USER = os.environ.get('AUTH_USERS', '777756062').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = "Obito™"#Here You Can Change with Your Name  or any custom name or title you prefer
+
 
 
